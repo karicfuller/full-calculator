6cae4939fbf5de1ff6998bc75625414c56f54ffe
kari and visetty <visetty@>
2018-05-30 19:32:17 -0700

    it will add business logic and userinterface to html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,40 @@
-/*Addition Functions*/
+// Business (or back-end) logic:
 
 var add = function(number1, number2) {
   return number1 + number2;
 };
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var division = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is user interface (or front-end) logic:
 
 var number1 = parseInt(prompt("Enter a number to add:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
 
-/*Subtraction functions*/
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
 
 var number1 = parseInt(prompt("Enter a number to subtract:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = subtract(number1, number2);
 alert(result);
 
-/*Multiplication Functions*/
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
 
 var number1 = parseInt(prompt("Enter a number to multiply:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = multiply(number1, number2);
 alert(result);
 
-/*Division Functions*/
-
-var division = function(number1, number2) {
-  return number1 / number2;
-};
 
 var number1 = parseInt(prompt("Enter a number to divide:"));
 var number2 = parseInt(prompt("Enter another number:"));
