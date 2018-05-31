e4cb9de01dae18cfa5b42ccc3537ed8828c46777
kari and visetty <kari@>
2018-05-30 19:53:37 -0700

    add number fields and provide sum and clear form

--- js/scripts.js
+++ js/scripts.js
@@ -18,25 +18,13 @@ var division = function(number1, number2) {
 
 // Everything below this line is user interface (or front-end) logic:
 
-var number1 = parseInt(prompt("Enter a number to add:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-
-
-var number1 = parseInt(prompt("Enter a number to subtract:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
-
-
-var number1 = parseInt(prompt("Enter a number to multiply:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
-
-
-var number1 = parseInt(prompt("Enter a number to divide:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = division(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+    $("form#add")[0].reset();    
+  });
+});
